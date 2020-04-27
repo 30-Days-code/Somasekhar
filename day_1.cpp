4595e0a12d4e01b3a032873a5e4ccb491bd81ca7
sekhar76405 <ssjio5402@gmail.com>
2020-04-27 21:05:54 +0530

    commiting with minimum declaring private members

---
+++ day_1.cpp
@@ -0,0 +1,42 @@
+// Define a class to represent a book in a library. 
+// Include the following members: -
+
+// Data Members: 
+// Book Number, Book Name, Author, Publisher, Price, No. of copies issued, No. of copies
+ 
+// Member Functions:
+// (i)	To assign initial values 
+// (ii)	To issue a book after checking for its availability 
+// (iii)	To return a book 
+// (iv)	To display book information
+
+#include <iostream>
+using namespace std;
+
+//creating a class named book
+class book
+{
+  char book_name[100]; //string for name
+  int book_number; 
+  float price;
+  char author[100];
+  char publisher[100];
+  int tot_cop,left_cop; //total copis and left copies.
+  
+  public:
+  
+  
+  
+};
+
+int main()
+{
+    int s;//for the switch follow
+    
+    cout<<"Hi User. Hope you are maintaining social distance.";
+    cout<<"\nEnter 1 to \n 2 to \n 3 to \n 4 to \n";
+    cin>>s;
+    switch
+
+    return 0;
+}
